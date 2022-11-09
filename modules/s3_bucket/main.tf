12a46fae9f0a62bd143fe7396339844782fae22f
Marcin Kaluza <mkaluz@amazon.com>
2022-11-09 12:24:46 +0000

    Added S3 module

---
+++ modules/s3_bucket/main.tf
@@ -0,0 +1,53 @@
+#
+# Terraform code bucket
+#
+resource "aws_s3_bucket" "bucket" {
+  #Skipping checkov checks
+  #checkov:skip=CKV_AWS_144: no s3 cross region replication
+  #checkov:skip=CKV_AWS_145: Ensure that S3 buckets are encrypted with KMS by default <- CloudFront requires S3 AES256
+  bucket_prefix = var.name_prefix
+  force_destroy = true
+}
+
+#
+# Versioning configuration
+#
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+#
+# Access logs
+#
+resource "aws_s3_bucket_logging" "example" {
+  bucket        = aws_s3_bucket.bucket.id
+  target_bucket = var.log_bucket != null ? var.log_bucket : aws_s3_bucket.bucket.id
+  target_prefix = "log/"
+}
+
+#
+# Terraform code bucket SSE conf
+#
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse_conf" {
+  bucket = aws_s3_bucket.bucket.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = var.sse_algorithm
+    }
+  }
+}
+
+#
+# Terraform code bucket public access conf
+#
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ modules/s3_bucket/variables.tf
@@ -0,0 +1,17 @@
+variable "name_prefix" {
+  type        = string
+  description = "Prefix for the S3 Bucket's name, ensuring it's full name is unique"
+
+}
+
+variable "sse_algorithm" {
+  type        = string
+  description = "SSE algorithm. Possible values are aws:kms or AES256"
+  default     = "aws:kms"
+}
+
+variable log_bucket {
+  type = string
+  description = "Target bucket for access logs (optional). If not provided, bucket will store log in itself"
+  default = null
+}
