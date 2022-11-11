0ce7db461b2c927912c69776fc80d8076bce3efa
Marcin Kaluza <mkaluz@amazon.com>
2022-11-11 10:55:47 +0000

    Fixed lambda policy

--- resources/main.tf
+++ resources/main.tf
@@ -3,17 +3,25 @@ module "sample" {
   name_prefix = "dummy-"
 }
 
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+    ]
+
+    resources = [
+      module.sample.arn
+    ]
+  }
+}
+
 module "lambda" {
   source = "../modules/lambda"
   function_name = "test"
   handler_name  = "app.handler"
   description   = "Test function"
-  resource_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = {
-      Action   = ["s3:Get*"]
-      Effect   = "Allow"
-      Resource = [module.sample.arn]
-    }
-  })
+  resource_policy = data.aws_iam_policy_document.json
 } 
