7c53ae6012c5f6336ae7bf7eb9e398bae065f5ef
Marcin Kaluza <mkaluz@amazon.com>
2022-11-11 10:29:51 +0000

    Initial implementation of the lambda module

--- resources/main.tf
+++ resources/main.tf
@@ -2,3 +2,18 @@ module "sample" {
   source      = "../modules/s3_bucket"
   name_prefix = "dummy-"
 }
+
+module "lambda" {
+  source = "../modules/lambda"
+  function_name = "test"
+  handler_name  = "app.handler"
+  description   = "Test function"
+  resource_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = {
+      Action   = ["s3:Get*"]
+      Effect   = "Allow"
+      Resource = [module.sample.arn]
+    }
+  })
+} 
