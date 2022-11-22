e3cc8b7ddf4880e7f37a7db42192185afe56f4ab
Marcin Kaluza <mkaluz@amazon.com>
2022-11-22 17:51:17 +0000

    Test with second vpc

--- resources/main.tf
+++ resources/main.tf
@@ -136,9 +136,18 @@ module "vpc" {
   source                      = "../modules/vpc"
   name                        = "Main VPC"
   cidr_block                  = "10.0.0.0/16"
-  public_subnets_cidr_blocks  = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
-  private_subnets_cidr_blocks = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
+  public_subnets_cidr_blocks  = ["10.0.1.0/24", "10.0.3.0/24" ]
+  private_subnets_cidr_blocks = ["10.0.2.0/24", "10.0.4.0/24"]
   interface_endpoint_services = ["ec2", "logs"]
   gateway_endpoint_services   = ["s3"]
 }
+
+module "vpc2" {
+  source                      = "../modules/vpc"
+  name                        = "Second VPC"
+  cidr_block                  = "10.0.0.0/16"
+  public_subnets_cidr_blocks  = ["10.0.1.0/24"]
+  private_subnets_cidr_blocks = ["10.0.2.0/24"]
+}
+
 
