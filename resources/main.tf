add28d75532fe28e49e6459b2ccf764e69ff1362
Marcin Kaluza <mkaluz@amazon.com>
2022-12-06 15:53:13 +0000

    Added chekcov skip to SG for EC2

--- resources/main.tf
+++ resources/main.tf
@@ -208,6 +208,7 @@ module "vpc2" {
 
 
 resource "aws_security_group" "private_ec2_sg" {
+  #   #checkov:skip=CKV2_AWS_5: "Ensure that Security Groups are attached to another resource"
   name_prefix = "private_ec2_security_group_"
   description = "SG for private EC2 instances"
   vpc_id      = module.vpc.vpc_id
