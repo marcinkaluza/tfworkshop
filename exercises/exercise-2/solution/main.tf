cc34c76f01a0a82939340fc270fd3ed26f184e69
Marine Haddad <marinehd@amazon.com>
2022-12-20 17:05:53 +0100

    changed duplicated resources

--- exercises/exercise-2/solution/main.tf
+++ exercises/exercise-2/solution/main.tf
@@ -34,14 +34,12 @@ resource "aws_internet_gateway" "igw" {
 # NOTE: It a requirement as per AWS Securiy Matrix have a NAT per AZ
 #
 resource "aws_eip" "nat_eip" {
-  count = length(var.public_subnets_cidr_blocks)
   vpc   = true
 }
 
 resource "aws_nat_gateway" "nat" {
-  count         = length(var.public_subnets_cidr_blocks)
-  subnet_id     = element(aws_subnet.public_subnet.*.id, count.index)
-  allocation_id = element(aws_eip.nat_eip.*.id, count.index)
+  subnet_id     = aws_subnet.public_subnet.id
+  allocation_id = aws_eip.nat_eip.id
 }
 
 
@@ -66,11 +64,10 @@ resource "aws_subnet" "private_subnet" {
 # to a NAT in the same AZ as the subnet.
 #
 resource "aws_route_table" "private_rt" {
-  count  = length(var.private_subnets_cidr_blocks)
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "Private Route Table ${count.index + 1}"
+    Name = "Private Route Table"
   }
 }
 
@@ -78,19 +75,17 @@ resource "aws_route_table" "private_rt" {
 # Route to the internet - different for each AZ/NAT gateway
 #
 resource "aws_route" "private_route_internet" {
-  count                  = var.allow_internet_egress ? length(var.private_subnets_cidr_blocks) : 0
-  route_table_id         = element(aws_route_table.private_rt.*.id, count.index)
+  route_table_id         = aws_route_table.private_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = element(aws_nat_gateway.nat.*.id, count.index)
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
 
 #
 # Association of route table with private subnet 
 #
 resource "aws_route_table_association" "private_route_table_association" {
-  count          = length(var.private_subnets_cidr_blocks)
-  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
-  route_table_id = element(aws_route_table.private_rt.*.id, count.index)
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
 }
 
 #
@@ -132,8 +127,7 @@ resource "aws_route" "public_route_internet" {
 # Association of the route table to all public subnets
 #
 resource "aws_route_table_association" "public_route_table_association" {
-  count          = length(var.public_subnets_cidr_blocks)
-  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
