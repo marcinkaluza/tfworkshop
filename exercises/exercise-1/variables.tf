3853a0e23777ca0e8f5b5543ff4b4ad91bfe08d2
Marcin Kaluza <mkaluz@amazon.com>
2023-01-06 11:21:38 +0000

    Cleanup of ex2

--- exercises/exercise-1/variables.tf
+++ exercises/exercise-1/variables.tf
@@ -1,4 +1,8 @@
 # Create variable of type string to contain target region 
 # https://developer.hashicorp.com/terraform/language/values/variables
 
+variable "target_region" {
+  type        = string
+  description = "Target region for deployment"
+}
 
