675270d344ad380ba89473ccef228865feac817a
Marcin Kaluza <mkaluz@amazon.com>
2023-01-06 10:39:49 +0000

    Modified starting point for exercise 2

--- exercises/exercise-2/main.tf
+++ exercises/exercise-2/main.tf
@@ -1,13 +1,9 @@
-data "aws_availability_zones" "available" {}
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-
+data "aws_availability_zones" "az" {}
 #
 # Creates a simple VPC using variable cidr_block and with DNS resolution enabled.
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
 #
 resource "aws_vpc" "main" {
-
 }
 
 #
@@ -15,7 +11,6 @@ resource "aws_vpc" "main" {
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/default_security_group
 #
 resource "aws_default_security_group" "default" {
-
 }
 
 #
@@ -23,7 +18,6 @@ resource "aws_default_security_group" "default" {
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
 #
 resource "aws_internet_gateway" "igw" {
-
 }
 
 #
@@ -31,49 +25,6 @@ resource "aws_internet_gateway" "igw" {
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
 #
 resource "aws_eip" "nat_eip" {
-
-}
-
-#
-# Creates the NAT Gateway in the VPC subnets using the variable public_subnets_cidr_blocks, and associate the EIP created above.
-# Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/nat_gateway
-#
-resource "aws_nat_gateway" "nat" {
-
-}
-
-
-
-#
-# Creates private subnets using the variable private_subnets_cidr_blocks for CIDR definition.
-# Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
-#
-resource "aws_subnet" "private_subnet" {
-
-}
-
-#
-# Creates private route table for all private subnets to route internet traffic to the NAT Gateway.
-# Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
-#
-resource "aws_route_table" "private_route_table" {
-
-}
-
-#
-# Creates a route to the NAT Gateway for egress traffic from the private subnets.
-# Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route
-#
-resource "aws_route" "private_route_internet" {
-
-}
-
-#
-# Associates of private route table with private subnets.
-# Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
-#
-resource "aws_route_table_association" "private_route_table_association" {
-
 }
 
 #
@@ -81,7 +32,7 @@ resource "aws_route_table_association" "private_route_table_association" {
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
 #
 resource "aws_subnet" "public_subnet" {
-
+  count = length(var.public_subnets_cidr_blocks)
 }
 
 #
@@ -89,7 +40,6 @@ resource "aws_subnet" "public_subnet" {
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
 #
 resource "aws_route_table" "public_route_table" {
-
 }
 
 #
@@ -97,7 +47,7 @@ resource "aws_route_table" "public_route_table" {
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route
 #
 resource "aws_route" "public_route_internet" {
-
+  gateway_id = aws_internet_gateway.igw.id
 }
 
 #
@@ -105,8 +55,48 @@ resource "aws_route" "public_route_internet" {
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
 #
 resource "aws_route_table_association" "public_route_table_association" {
-
+  count = length(var.public_subnets_cidr_blocks)
 }
+
+#
+# Creates the NAT Gateway in the VPC subnets using the variable public_subnets_cidr_blocks, and associate the EIP created above.
+# Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/nat_gateway
+#
+# resource "aws_nat_gateway" "nat" {
+# }
+
+#
+# Creates private subnets using the variable private_subnets_cidr_blocks for CIDR definition.
+# Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+#
+# resource "aws_subnet" "private_subnet" {
+#   count             = length(var.private_subnets_cidr_blocks)
+# }
+
+#
+# Creates private route table for all private subnets to route internet traffic to the NAT Gateway.
+# Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
+#
+# resource "aws_route_table" "private_route_table" {
+# }
+
+#
+# Creates a route to the NAT Gateway for egress traffic from the private subnets.
+# Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route
+#
+# resource "aws_route" "private_route_internet" {
+#   nat_gateway_id         = aws_nat_gateway.nat.id
+# }
+
+#
+# Associates of private route table with private subnets.
+# Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+#
+# resource "aws_route_table_association" "private_route_table_association" {
+#   count          = length(var.private_subnets_cidr_blocks)
+# }
+
+
 
 
 
