da5fb02846c77a3cdb9f962d7499ae41d6a6b4b5
Marcin Kaluza <mkaluz@amazon.com>
2023-01-09 22:04:17 +0000

    Revert "Revert "Added solution to exercise 5""
    This reverts commit 8af7fdaeee697a589ecab6ae60cb3f9c3571029b.
    
    Solution is back

--- exercises/exercise-5/solution/modules/autoscaling-group/main.tf
+++ exercises/exercise-5/solution/modules/autoscaling-group/main.tf
@@ -3,52 +3,74 @@
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
 #
 resource "aws_lb" "load_balancer" {
-  # Set following properties of the load balancer:
-  # name
-  # internal (set to true)
-  # type of load balancer needs to be set to "network"
-  # subnets argument needs to be provided and set to 
-  # the value of the corresponding variable
+  name               = "web-nlb"
+  internal           = true
+  load_balancer_type = "network"
+  subnets            = var.subnets
 }
 
 # Creates a target group to attach to the load balancer, accessible on port HTTP:80 and located in the existing VPC
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
 #
 resource "aws_lb_target_group" "target_group" {
-  # Following arguments are required:
-  # name
-  # id of the vpc
-  # port (set to 80)
-  # protocol set to "TCP"
+  name     = "web-nlb-target-group"
+  vpc_id   = var.vpc
+  port     = 80
+  protocol = "TCP"
 }
 
 # Creates a listener for the load balancer to listen on port TCP:80, with a default action to forward to its target group
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
 #
 resource "aws_lb_listener" "listener" {
-  # Following arguments required:
-  # ARN of the load balancer
-  # port (set to 80)
-  # protocol (set to "TCP")
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = "80"
+  protocol          = "TCP"
 
-  # Forward all traffic to the target group
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.target_group.arn
   }
 }
 
+resource "aws_iam_role" "asg_iam_role" {
+  name_prefix = "asg-iam-role-"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+data "aws_iam_policy" "ssm" {
+  name = "AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.asg_iam_role.name
+  policy_arn = data.aws_iam_policy.ssm.arn
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name_prefix = "asg_instance_profile_"
+  role        = aws_iam_role.asg_iam_role.name
+}
+
+
 # Creates launch configuration for the autoscaling group, with same arguments as for the Bastion EC2 instance
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_configuration
 #
 resource "aws_launch_configuration" "launch_configuration" {
-  # Following arguments are required 
-  # All arguments have corresponding variables
-  # Id of the AMI
-  # user_data for the instance
-  # instance_type
-  # security groups
-  
+  image_id             = var.ami_id
+  user_data            = var.user_data
+  instance_type        = var.instance_type
+  security_groups      = var.asg_security_groups
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
   lifecycle {
     create_before_destroy = true
@@ -60,14 +82,13 @@ resource "aws_launch_configuration" "launch_configuration" {
 # Documentation : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_group
 #
 resource "aws_autoscaling_group" "asg" {
-  # Following arguments are required:
-  # name
-  # max size of the autoscaling group (set to 5)
-  # min size of the autoscaling group (set to 1)
-  # health check grace period (set to 300)
-  # health check type (set to "ELB")
-  # desired capacity (set to 1)
-  # force delete (set to True)
+  name                      = "web-server-asg"
+  max_size                  = 5
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = true
   launch_configuration      = aws_launch_configuration.launch_configuration.name
   vpc_zone_identifier       = var.subnets
   target_group_arns         = [aws_lb_target_group.target_group.arn]
