a14d878027a5767329495a70bc52bac63915a995
Marcin Kaluza <mkaluz@amazon.com>
2023-01-11 11:26:00 +0000

    region set to us-east-1

--- exercises/exercise-0/hello.tf
+++ exercises/exercise-0/hello.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
   default_tags {
     tags = {
       Project       = "A project"
--- exercises/exercise-5/solution/variables.tf
+++ exercises/exercise-5/solution/variables.tf
@@ -1,5 +1,5 @@
 variable "target_region" {
   type        = string
   description = "Target region"
-  default     = "eu-west-1"
+  default     = "us-east-1"
 }
